4e3cd1bb374a6bb21c569bfd66fcdb713dd6e753
socks2309 <sock.lord2309@gmail.com>
2023-10-13 21:58:04 +0530

    small changes

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@
 ##############################################################################
 
 # Read Bambu path
-read -p "Enter Bambu AppImage as path (Eg. /home/{user}/bambu-0.9.7.AppImage): " bambu_path 
+read -p "Enter Bambu AppImage as path (Eg. /home/{user}/bambu-0.9.7.AppImage): " bambu_path
 
 # Run ANN model script on Python
 python3 ~/neural-network-fpga/main-branch/src/python/ann.py
@@ -15,31 +15,28 @@ python3 ~/neural-network-fpga/main-branch/src/python/ann.py
 # Removed previously ran Bambu HLS files
 cd ~/neural-network-fpga/main-branch/src/hdl/
 if test -f network.v; then
-    mv network_tb.v ../
-    cd ..
-    rm -r hdl
-    mkdir hdl && mv network_tb.v hdl/
+	mv network_tb.v ../
+	cd ..
+	rm -r hdl
+	mkdir hdl && mv network_tb.v hdl/
 fi
 
-# Calling Bambu tool
-cd ~/neural-network-fpga/main-branch/src/hdl/ && $bambu_path ~/neural-network-fpga/main-branch/src/c/ann.c --top-fname=network --fp-subnormal -lm
-
-# Icarus Verilog compilation 
+# Icarus Verilog compilation
 iverilog -o result network.v network_tb.v
 vvp result
 
 # Building C files and viewing C result and Keras results
 cd ~/neural-network-fpga/main-branch/
 if [ -d "build" ]; then
-    rm -rf build
-    mkdir build && cd ~/neural-network-fpga/main-branch/build/
-    cmake ../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE && make
+	rm -rf build
+	mkdir build && cd ~/neural-network-fpga/main-branch/build/
+	cmake ../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE && make
 fi
 
 mkdir build && cd ~/neural-network-fpga/main-branch/build/
 cmake ../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE && make
 
 # Icarus Verilog compilation
-cd ~/neural-network-fpga/main-branch/src/hdl/ 
+cd ~/neural-network-fpga/main-branch/src/hdl/
 iverilog -o result network.v network_tb.v
 vvp result
